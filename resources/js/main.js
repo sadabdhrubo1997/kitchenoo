8869f7c89154b13ab0e2c6dc5e023b17ff866c16
sadabdhrubo1997 <53494264+sadabdhrubo1997@users.noreply.github.com>
2020-12-03 10:34:52 +0600

    add second

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,36 @@
 $(document).ready(function(){
 
+
+  $(window).on('resize', function(){
+    var iw = window.innerWidth;
+    var rootScreenWidth = 1366;
+    var rootFontPercentage = 44.46;     
+   
+    if (iw <= 1366) {
+        $('#my_html').attr("style", `font-size:${rootFontPercentage}%`);
+        
+    }else{
+        let currentFontSize = (rootFontPercentage * iw) / rootScreenWidth;
+        $('#my_html').attr("style", `font-size:${currentFontSize}%`);    
+    }
+  })
+  
+
+    // Responsive html font size
+    var iw = window.innerWidth;
+    var rootScreenWidth = 1366;
+    var rootFontPercentage = 44.46;     
+   
+    if (iw <= 1366) {
+        $('#my_html').attr("style", `font-size:${rootFontPercentage}%`);
+        
+    }else{
+        let currentFontSize = (rootFontPercentage * iw) / rootScreenWidth;
+        $('#my_html').attr("style", `font-size:${currentFontSize}%`);    
+    }
+        
+    // Responsive html font size end
+
     $('#testimonial_slider').slick({
         dots: false,
         infinite: true,
